d9048b8bbcd79f38e4452cd7407900fbb3840f9b
Marcello Figueiredo <marcello.figueiredo@solintel.local>
2025-03-07 16:55:54 -0300

    :tada: initial commit

---
+++ js/start.js
@@ -0,0 +1,4 @@
+document.getElementById("start-btn").addEventListener("click", function() {
+    // Redireciona para a página do jogo (index.html)
+    window.location.href = "../index.html";  // Corrige para navegar para o arquivo index.html
+});
